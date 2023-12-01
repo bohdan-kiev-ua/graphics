0ff617c9284694adb1ea5f8fef5b04e9987ee518
bohdan-kiev-ua <bohdan.kiev.ua@gmail.com>
2023-12-01 14:33:05 +0200

    first commit 1.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,87 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import MenuItem from "@/app/components/MenuItem/MenuItem";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+    <main className={styles.container}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+            src="/images/header-bg.png"
+            alt=""
+            className={styles.headerImage}
+            width={0}
+            height={0}
+            sizes="100%"
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className={styles.profile}>User</div>
+        <div className={styles.flexWrapper}>
+            <div className={styles.sidebar}>
+                <div>
+                    <div className={styles.sidebarTitle}>Good x (Morning, Afternoon, Evening, User</div>
+                    <div className={styles.sidebarMenu}>
+                        <MenuItem text={'Dashboard'} icon={'/icons/dasboard.svg'} />
+                        <MenuItem text={'Tasks'} icon={'/icons/tasks.svg'} />
+                        <MenuItem text={'Map'} icon={'/icons/map.svg'} />
+                        <MenuItem text={'Message'} icon={'/icons/message.svg'} messages={3}/>
+                        <MenuItem text={'Calendar'} icon={'/icons/calendar.svg'} />
+                    </div>
+                </div>
+                <div className={styles.teamContainer}>
+                    <div className={styles.teamData}>
+                        <div className={styles.teamIcon} />
+                        <div className={styles.teamDataRight}>
+                            <div>
+                                <div className={styles.teamTitle}>Team Echo</div>
+                                <div className={styles.teamSubtitle}>5 people</div>
+                            </div>
+                            <div className={styles.teamUpdatesContainer}>
+                                <div className={styles.teamUpdates}>See Update</div>
+                                <Image
+                                    src="/icons/orangeLine.svg"
+                                    alt=""
+                                    className={styles.line}
+                                    width={0}
+                                    height={0}
+                                    sizes="100%"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <Image
+                        src="/icons/arrowLink.svg"
+                        alt=""
+                        className={styles.line}
+                        width={0}
+                        height={0}
+                        sizes="100%"
+                    />
+                </div>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.contentGraphRow}>
+                    <div className={styles.graphContainer}></div>
+                    <div className={styles.dailyGraphContainer}>
+                        <div className={styles.dailyGraph}></div>
+                        <div className={styles.dailyGraph}></div>
+                    </div>
+                    <div className={styles.totalGraphContainer}>
+                        <div className={styles.totalTasks}></div>
+                        <div className={styles.todayGraph}></div>
+                    </div>
+                </div>
+                <div className={styles.mapContainer}>
+                    <div>
+                        <div className={styles.criticalConditions}></div>
+                        <div className={styles.developersContainer}>
+                            <div className={styles.developers}></div>
+                            <div className={styles.developers}></div>
+                            <div className={styles.developers}></div>
+                        </div>
+                    </div>
+                    <div className={styles.map}></div>
+                </div>
+            </div>
+        </div>
     </main>
   )
 }
